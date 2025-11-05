117512d2857c155a2a8dbf3697bceb734c129f64
kenzyalonzo <kenryalonzo@gmail.com>
2025-11-05 10:30:43 +0100

    Add navigation bar, event listing, and explore button components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
 
 const page = () => {
   return (
@@ -9,6 +11,18 @@ const page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, and Conferences, All in One Place
       </p>
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
